f76ec9468ef2a7cb84c680a7e8487b455f9e8589
Navneesh Yadav <yadavnavneesh999@gmail.com>
2023-04-06 14:11:33 +0530

    infinite feature completed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,35 @@ import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
 export default class App extends Component {
+
   pageSize=15
+  apiKey = process.env.REACT_APP_NEWS_API
+  state = {
+    progress:0
+  }
+
+  setProgress = (progress)=>{
+    this.setState({progress: progress})
+  }
   render() {
     return (
       <div>
         <Router>
           <NavBar />
+          <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={this.state.progress} 
+      /> 
           <Routes>
             <Route
               exact
               path="/"
               element={
-                <News
+                <News setProgress={this.setProgress} apiKey={this.apiKey}
                   key="general"
                   pageSize={this.pageSize}
                   country="in"
@@ -28,7 +44,7 @@ export default class App extends Component {
               exact
               path="/business"
               element={
-                <News
+                <News setProgress={this.setProgress} apiKey={this.apiKey}
                   key="business"
                   pageSize={this.pageSize}
                   country="in"
@@ -40,7 +56,7 @@ export default class App extends Component {
               exact
               path="/entertainment"
               element={
-                <News
+                <News setProgress={this.setProgress} apiKey={this.apiKey}
                   key="entertainment"
                   pageSize={this.pageSize}
                   country="in"
@@ -52,7 +68,7 @@ export default class App extends Component {
               exact
               path="/general"
               element={
-                <News
+                <News setProgress={this.setProgress} apiKey={this.apiKey}
                   key="general"
                   pageSize={this.pageSize}
                   country="in"
@@ -64,7 +80,7 @@ export default class App extends Component {
               exact
               path="/health"
               element={
-                <News
+                <News setProgress={this.setProgress} apiKey={this.apiKey}
                   key="health"
                   pageSize={this.pageSize}
                   country="in"
@@ -76,7 +92,7 @@ export default class App extends Component {
               exact
               path="/science"
               element={
-                <News
+                <News setProgress={this.setProgress} apiKey={this.apiKey}
                   key="science"
                   pageSize={this.pageSize}
                   country="in"
@@ -88,7 +104,7 @@ export default class App extends Component {
               exact
               path="/sports"
               element={
-                <News
+                <News setProgress={this.setProgress} apiKey={this.apiKey}
                   key="sports"
                   pageSize={this.pageSize}
                   country="in"
@@ -100,7 +116,7 @@ export default class App extends Component {
               exact
               path="/technology"
               element={
-                <News
+                <News setProgress={this.setProgress} apiKey={this.apiKey}
                   key="technology"
                   pageSize={this.pageSize}
                   country="in"
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import loading from './loading.gif'
 export default class spinner extends Component {
   render() {
     return (
-      <div className="text-center"><img src={loading} alt="loading" /></div>
+      <div className="text-center"><img className="my-3" src={loading} alt="loading" /></div>
     )
   }
 }
